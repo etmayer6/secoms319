94c026629b4ab1cf4ea1990486810f9b5d326c75
etmayer6 <etmayer6@gmail.com>
2023-10-13 16:37:02 -0500

    Added cool boxes, more weather info

--- page2/index.js
+++ page2/index.js
@@ -1,6 +1,11 @@
 
 
 var temperature;
+var feelslike;
+var precipprob;
+var preciptype;
+var windspeed;
+var humidity;
 
 
 fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/42.034534%2C-93.620369?unitGroup=metric&key=K6SLSU6G3HZ7JJ8DGGGMQDVBK&contentType=json")
@@ -10,9 +15,17 @@ fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/service
 function findTemperature(weatherData){
 
     temperature = weatherData.days[0].temp;
-    
+    feelslike = weatherData.days[0].feelslike;
+    precipprob = weatherData.days[0].precipprob;
+    preciptype = weatherData.days[0].preciptype[0];
+    windspeed = weatherData.days[0].windspeed;
+    humidity = weatherData.days[0].humidity;
     //convert to farenheit
     temperature = (temperature * 9/5) + 32;
+    feelslike = (feelslike * 9/5) + 32;
+    //convert to mph
+    windspeed = Math.ceil(windspeed/1.609);
+    
 
     fetch("data.json")
     .then(response => response.json())
@@ -30,7 +43,7 @@ function loadWeather(weather){
     var tempWeather = document.getElementById("weather");
     var imgWeather = document.getElementById("image");
     var txtWeather = document.getElementById("text");
-
+    var moreinfoWeather = document.getElementById("moreinfo");
     //temperature = weatherData.days[0].temp;
 
 let index;
@@ -84,7 +97,7 @@ let image = weather.weather[index].image;
 let text = weather.weather[index].text;
 
 let outside = document.createElement("p");
-outside.innerHTML = `<p class="coolfont"> Current temperature in Ames: <strong>${temperature}</strong> <br> 
+outside.innerHTML = `<p class="coolfont"> Current temperature in Ames: <strong>${temperature}</strong> <br><br> 
 Based off the temperature outside, your view may look something like this: </p>`;
 
 let img = document.createElement("div");
@@ -93,9 +106,20 @@ img.innerHTML = `<img src=${image} class="imagechanger"></img>`;
 let txt = document.createElement("p");
 txt.innerHTML = `<p class="coolfont"> The climate outside feels: <strong>${title}</strong>, ${text}</p>`;
 
+
+let info = document.createElement("p");
+info.innerHTML = `<p class="coolfont"> Feels like: <strong>${feelslike}</strong><br>
+                                    Humidity: <strong>${humidity}</strong>% <br>
+                                    Probability of precipitation: <strong>${precipprob}</strong>% <br>
+                                    Type of precipitation: <strong>${preciptype}</strong> <br>
+                                    Windspeed: <strong>${windspeed}</strong> MPH <br>
+
+</p>`
+
 tempWeather.appendChild(outside);
 imgWeather.appendChild(img);
 txtWeather.appendChild(txt);
+moreinfoWeather.appendChild(info);
 
 index = 0;
 
