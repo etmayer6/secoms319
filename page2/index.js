d98ce4712f3fa8d026036182e379e07ee88d7949
etmayer6 <etmayer6@gmail.com>
2023-10-13 10:44:05 -0500

    fixed local weather fetch

--- page2/index.js
+++ page2/index.js
@@ -1,19 +1,41 @@
 
-fetch("data.json")
+
+var temperature;
+
+
+fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/42.034534%2C-93.620369?unitGroup=metric&key=K6SLSU6G3HZ7JJ8DGGGMQDVBK&contentType=json")
 .then(response => response.json())
-.then(weather => loadWeather(weather));
+.then(weatherData => findTemperature(weatherData));
+
+function findTemperature(weatherData){
+
+    temperature = weatherData.days[0].temp;
+    
+    //convert to farenheit
+    temperature = (temperature * 9/5) + 32;
+
+    fetch("data.json")
+    .then(response => response.json())
+    .then(weather => loadWeather(weather));
+
+}
+
+
+
+
 
 function loadWeather(weather){
 
 
-
-
+    var tempWeather = document.getElementById("weather");
     var imgWeather = document.getElementById("image");
     var txtWeather = document.getElementById("text");
 
+    //temperature = weatherData.days[0].temp;
 
-let temperature = 21;//need to assign temperature to the input value of the raspberry pi
 let index;
+
+console.log(temperature);
 
 if(temperature < 0){
     index = 0;
@@ -61,12 +83,17 @@ let title = weather.weather[index].title;
 let image = weather.weather[index].image;
 let text = weather.weather[index].text;
 
+let outside = document.createElement("p");
+outside.innerHTML = `<p class="coolfont"> Current temperature in Ames: <strong>${temperature}</strong> <br> 
+Based off the temperature outside, your view may look something like this: </p>`;
+
 let img = document.createElement("div");
 img.innerHTML = `<img src=${image} class="imagechanger"></img>`;
 
 let txt = document.createElement("p");
 txt.innerHTML = `<p class="coolfont"> <strong>${title}</strong>, ${text}</p>`;
 
+tempWeather.appendChild(outside);
 imgWeather.appendChild(img);
 txtWeather.appendChild(txt);
 
