dd9d8757f4cf98e11d1bc37bdfe163c1d9bd58b5
Cal <calhf@iastate.edu>
2023-11-02 18:51:56 -0500

    bootstrap working for catalog page

--- project2/src/Shopping.js
+++ project2/src/Shopping.js
@@ -15,7 +15,6 @@ const Shop = () => {
     setCartTotal(totalVal);
   };
 
-  
   const addToCart = (el) => {
     setCart([...cart, el]);
   };
@@ -24,15 +23,40 @@ const Shop = () => {
     hardCopy = hardCopy.filter((cartItem) => cartItem.id !== el.id);
     setCart(hardCopy);
   };
-
+  function howManyofThis(id) {
+    let hmot = cart.filter((cartItem) => cartItem.id === id);
+    return hmot.length;
+    }
   const listItems = items.map((el) => (
-    <div key={el.id}>
-      <img className="img-fluid" src={el.image} width={150} /> <br />
-      {el.title} <br />
-      {el.category} <br />
-      {el.price} <br />
-      <button type="button" onClick={() => removeFromCart(el)}>-</button>{" "}
-      <button type="button" variant="light" onClick={() => addToCart(el)}>{" "}+{" "}</button>
+    // PRODUCT
+    <div class="row border-top border-bottom" key={el.id}>
+      <div class="row main align-items-center">
+        <div class="col-2">
+          <img class="img-fluid" src={el.image} />
+        </div>
+        <div class="col">
+          <div class="row text-muted">{el.title}</div>
+          <div class="row">{el.category}</div>
+        </div>
+        <div class="col">
+          <button
+            type="button"
+            variant="light"
+            onClick={() => removeFromCart(el)}
+          >
+            {" "}
+            -{" "}
+          </button>{" "}
+          <button type="button" variant="light" onClick={() => addToCart(el)}>
+            {" "}
+            +{" "}
+          </button>
+        </div>
+        <div class="col">
+          ${el.price} <span class="close">&#10005;</span>
+          {howManyofThis(el.id)}
+        </div>
+      </div>
     </div>
   ));
   const cartItems = cart.map((el) => (
